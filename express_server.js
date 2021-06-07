306be2a3e1a77bb40b79e5d82e79c0b837368603
mandatoryrecess <rossanneclamp@gmail.com>
2021-06-07 22:31:17 +0000

    urls_new added, GET and POST routs

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -16,12 +17,21 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+//urls_new
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 
 //URLS 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase}; 
   res.render("urls_index", templateVars)
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 //URLS SHOW 
@@ -36,3 +46,8 @@ app.get("/urls/:shortURL", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  const randomString = Math.random().toString(36).substring(2, 8);
+  return randomString;
+}
