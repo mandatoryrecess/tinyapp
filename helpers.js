3d49b38f1fc8a068d72e728251f48f9da4e37c5b
mandatoryrecess <rossanneclamp@gmail.com>
2021-06-11 01:35:56 +0000

    debugging

--- helpers.js
+++ helpers.js
@@ -1,7 +1,6 @@
 const findUserByEmail = function (email, userDatabase) {
   for (let userID in userDatabase) {
     const user = userDatabase[userID];
-    console.log(user);
     if (user.email === email) {
       return user;
     }
